965fef71c40f6d6a3a32410bbc8bdc8bf4cdd54b
Ian Ramos <ianramossantos@hotmail.com>
2023-11-30 12:33:41 -0300

    feat: metodos para buscar/adicionar usuarios e livros na lista

--- src/Biblioteca/Biblioteca.py
+++ src/Biblioteca/Biblioteca.py
@@ -1,14 +1,39 @@
 from ..Livro import Livro
+from ..Usuario import Usuario
 
 
 class Biblioteca:
     def __init__(self):
-        self.__livros = []
+        self.__livros: list[Livro] = []
+        self.__usuarios: list[Usuario] = []
 
-    def adicionar_livro(self, livro: Livro):
-        self.__livros.append(livro)
+    def adicionarLivro(self, novoLivro: Livro, qtdExemplares: int) -> None:
+        livroEncontrado = self.__buscarLivroPeloCodigo(novoLivro.getCodigo())
 
-    def realizarEmprestimo(self):
+        if livroEncontrado:
+            livroEncontrado.setQtdDisponivel(qtdExemplares)
+            return
+        else:
+            novoLivro.setQtdDisponivel(qtdExemplares)
+            self.__livros.append(novoLivro)
+
+    def __buscarLivroPeloCodigo(self, codigoLivro: int) -> Livro:
+        for livro in self.__livros:
+            if livro.getCodigo() == codigoLivro:
+                return livro
+
+    def adicionarUsuario(self, novoUsuario: Usuario) -> None:
+        usuarioEncontrado = self.__buscarUsuarioPeloCodigo(novoUsuario.getCodigo())
+
+        if usuarioEncontrado == None:
+            self.__usuarios.append(novoUsuario)
+
+    def __buscarUsuarioPeloCodigo(self, codigoUsuario: int) -> Usuario:
+        for usuario in self.__usuarios:
+            if usuario.getCodigo() == codigoUsuario:
+                return usuario
+
+    def realizarEmprestimo(self) -> None:
         # Verificar se o usuário possui reserva para o livro
         # Se possuir reserva, exclui e realiza o emprestimo
         # Se não possuir, verificar se há disponibilidade do livro e realiza o emprestimo
