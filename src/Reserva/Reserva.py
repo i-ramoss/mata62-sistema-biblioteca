1179763ef8c77cb76c21898ffe5b05185a47482b
Ian Ramos <ianramossantos@hotmail.com>
2023-12-01 19:24:21 -0300

    feat: classe de reserva

--- src/Reserva/Reserva.py
+++ src/Reserva/Reserva.py
@@ -1,8 +1,14 @@
 from ..Usuario import Usuario
-from ..Livro import Livro 
+from ..Livro import Livro
 
-class Reserva():
+
+class Reserva:
     def __init__(self, usuario: Usuario, livro: Livro):
         self.__usuario = usuario
-        self.__livro = livro 
+        self.__livro = livro
 
+    def getUsuario(self):
+        return self.__usuario
+
+    def getLivro(self):
+        return self.__livro
---
+++ src/Reserva/__init__.py
@@ -0,0 +1,1 @@
+from .Reserva import Reserva
