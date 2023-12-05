8c85d5ba1888e76107acc14afda63d0e28957fec
Ian Ramos <ianramossantos@hotmail.com>
2023-12-05 08:06:59 -0300

    fix: construtor do professor

--- Main.py
+++ Main.py
@@ -1,23 +1,56 @@
-from src import AlunoGraduacao, BibliotecaSingletonFacade, Livro
-
-
-usuario1 = AlunoGraduacao(123, "Flora Ramos")
-usuario2 = AlunoGraduacao(456, "Cecília Ramos")
-
-engSoftSommervile = Livro(
-    100, "Engenharia de Software", "AddisonWesley", ["Ian Sommmervile"], "6ª", 2000
+from src import (
+    AlunoGraduacao,
+    AlunoPosGraduacao,
+    Professor,
+    BibliotecaSingletonFacade,
+    Livro,
+    ConsoleSingleton,
 )
+
+usuarios = [
+    AlunoGraduacao(123, "Flora Ramos"),
+    AlunoGraduacao(456, "Cecília Ramos"),
+    AlunoPosGraduacao(789, "Ninna Simas"),
+    Professor("100", "Carlos Sant'Anna"),
+]
+
+livros = [
+    {
+        "codigo": 100,
+        "titulo": "Engenharia de Software",
+        "editora": "AddisonWesley",
+        "autores": ["Ian Sommmervile"],
+        "edicao": "6ª",
+        "anoPublicacao": 2000,
+        "qtdExemplares": 5,
+    }
+]
 
 biblioteca = BibliotecaSingletonFacade()
 
-biblioteca.adicionarUsuario(usuario1)
-biblioteca.adicionarUsuario(usuario2)
-biblioteca.adicionarLivro(engSoftSommervile, 2)
+for usuario in usuarios:
+    biblioteca.adicionarUsuario(usuario)
 
-biblioteca.realizarReserva(usuario1.getCodigo(), engSoftSommervile.getCodigo())
-biblioteca.realizarEmprestimo(usuario1.getCodigo(), engSoftSommervile.getCodigo())
-biblioteca.realizarEmprestimo(usuario2.getCodigo(), engSoftSommervile.getCodigo())
+for livro in livros:
+    novoLivro = Livro(
+        livro.get("codigo"),
+        livro.get("titulo"),
+        livro.get("editora"),
+        livro.get("autores"),
+        livro.get("edicao"),
+        livro.get("anoPublicacao"),
+    )
+
+    biblioteca.adicionarLivro(novoLivro, livro.get("qtdExemplares"))
+
+
+# biblioteca.realizarReserva(usuario1.getCodigo(), engSoftSommervile.getCodigo())
+# biblioteca.realizarEmprestimo(usuario1.getCodigo(), engSoftSommervile.getCodigo())
+# biblioteca.realizarEmprestimo(usuario2.getCodigo(), engSoftSommervile.getCodigo())
 # biblioteca.realizarDevolucao(usuario2.getCodigo(), engSoftSommervile.getCodigo())
 
-biblioteca.consultarLivro(engSoftSommervile.getCodigo())
+# biblioteca.consultarLivro(engSoftSommervile.getCodigo())
 # biblioteca.consultarUsuario(usuario1.getCodigo())
+
+console = ConsoleSingleton()
+console.getConsoleLoop()
